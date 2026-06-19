f837d986d588aa5144b522ebd5ed9e7cda86c654
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2026-06-19 11:42:15 -0400

    feat: ambiente configurado; esqueleto da pagina de listagem; css customizado iniciado; tabela iniciado;

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hidrometeorologia',
+        'USER': 'CRPV',
+        'HOST': '172.22.5.54',
+        'PASSWORD': '1eD6t9QB',
+        'PORT': '5432',
     }
 }
 
@@ -115,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from core.models import InundacaoUrbana
+from django.forms.models import model_to_dict
+import json
 # Create your views here.
 
-def view_listagem_dados(request):
-    return render(request, 'core/template_listagem_dados.html')
+def view_listagem_dados(request):    
+    dados = InundacaoUrbana.objects.all()
+    dados_json = [
+        model_to_dict(item)
+        for item in dados
+    ]
+
+    num_dados = len(dados)
+    num_estacoes = sum(1 for item in dados if item.cd_estacao)
+    num_municipios = len(set(item.municipio for item in dados))
+
+    num_integrado = sum(1 for item in dados if item.integrado)
+    num_cota_atencao = sum(1 for item in dados if item.cota_atencao)
+    num_cota_alerta = sum(1 for item in dados if item.cota_alerta)
+    num_cota_inundacao = sum(1 for item in dados if item.cota_inundacao)
+
+    if num_dados > 0 :
+        por_cent_integrado = (num_integrado * 100) / num_dados
+    else:
+        por_cent_integrado = 0.0
+
+
+    return render(request, 
+                  'core/template_listagem_dados.html',
+                  {
+                    'dados': dados,
+                    'dados_json': dados_json,
+                    'num_dados': num_dados, 
+                    'num_municipios': num_municipios,
+                    'num_estacoes': num_estacoes,
+                    'num_integrado': num_integrado,
+                    'num_cota_atencao': num_cota_atencao,
+                    'num_cota_alerta': num_cota_alerta,
+                    'num_cota_inundacao': num_cota_inundacao,
+                    'por_cent_integrado': por_cent_integrado
+                  }
+                )
